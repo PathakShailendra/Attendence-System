6fb6e59145c4e31fcdc3cdfa7791b11767ac0149
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-14 17:28:39 +0530

    database connected

---
+++ Backend/Index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Attendance System API is running...");
+});
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+});
