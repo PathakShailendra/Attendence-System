2f48217d1213f24be06ee16f686bcc9cacba31c1
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-04-17 11:33:57 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://attendence-system-backend-mk10.onrender.com", // your frontend URL
+    origin: "https://attendence-system-frontend.onrender.com", // your frontend URL
     credentials: true, // to allow cookies, tokens, headers, etc.
   })
 );
