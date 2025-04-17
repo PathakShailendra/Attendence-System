87d7bef2c6d9b36e5e3618e6616701081bf2cf31
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-04-17 11:04:31 +0530

    Update Axios.js

--- Frontend/src/utils/Axios.js
+++ Frontend/src/utils/Axios.js
@@ -7,7 +7,7 @@ const getToken = () => {
 };
 
 const Axios = axios.create({
-  baseURL: "http://localhost:3000", 
+  baseURL: "https://attendence-system-backend-mk10.onrender.com", 
   withCredentials: true,// ✅ Updated base URL
   headers: {
     "Content-Type": "application/json",
